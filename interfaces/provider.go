c525b2d88399da8b2750b9b191843020fd585923
dayong <wangdayong228@163.com>
2022-05-16 15:36:43 +0800

    add provider wrapper

---
+++ interfaces/provider.go
@@ -0,0 +1,16 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/openweb3/go-rpc-provider"
+)
+
+type Provider interface {
+	Call(resultPtr interface{}, method string, args ...interface{}) error
+	CallContext(ctx context.Context, result interface{}, method string, args ...interface{}) error
+	BatchCall(b []rpc.BatchElem) error
+	BatchCallContext(ctx context.Context, b []rpc.BatchElem) error
+	Subscribe(ctx context.Context, namespace string, channel interface{}, args ...interface{}) (*rpc.ClientSubscription, error)
+	Close()
+}
