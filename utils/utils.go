658f122657417aeff7e3706470d5406eca24dc27
dayong <wangdayong228@163.com>
2022-05-25 18:18:21 +0800

    set default request timeout to 30s

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"reflect"
 
 	"github.com/pkg/errors"
@@ -14,4 +16,15 @@ func IsRPCJSONError(err error) bool {
 
 	t := reflect.TypeOf(errors.Cause(err)).String()
 	return t == "*rpc.jsonError" || t == "rpc.jsonError" || t == "utils.RpcError" || t == "*utils.RpcError"
+}
+
+// PrettyJSON json marshal value and pretty with indent
+func PrettyJSON(value interface{}) string {
+	j, e := json.Marshal(value)
+	if e != nil {
+		panic(e)
+	}
+	var str bytes.Buffer
+	_ = json.Indent(&str, j, "", "    ")
+	return str.String()
 }
