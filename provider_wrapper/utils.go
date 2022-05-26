c7a98f49df2312d5f5d6c1605486d45af2f06a93
dayong <wangdayong228@163.com>
2022-05-26 11:03:47 +0800

    nil

--- provider_wrapper/utils.go
+++
@@ -1,8 +0,0 @@
-package providers
-
-import "github.com/openweb3/go-rpc-provider/interfaces"
-
-func IsMiddlewareableProvider(p interfaces.Provider) bool {
-	_, ok := p.(*MiddlewarableProvider)
-	return ok
-}
