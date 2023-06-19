bfe27b6e147a4949c160b1c95994bf3eb50961d3
Péter Garamvölgyi <peter@scroll.io>
2023-06-19 10:45:34 +0200

    change null behaviour to invalid request

--- testdata/reqresp-batch.js
+++ testdata/reqresp-batch.js
@@ -6,6 +6,3 @@
 
 --> [{"jsonrpc":"2.0","id":2,"method":"test_echo","params":[]}, {"jsonrpc":"2.0","id": 3,"method":"test_echo","params":["x",3]}]
 <-- [{"jsonrpc":"2.0","id":2,"error":{"code":-32602,"message":"missing value for required argument 0"}},{"jsonrpc":"2.0","id":3,"result":{"String":"x","Int":3,"Args":null}}]
-
---> [null, {"jsonrpc":"2.0","id": 3,"method":"test_echo","params":["x",3]}]
-<-- [{"jsonrpc":"2.0","id":3,"result":{"String":"x","Int":3,"Args":null}}]
